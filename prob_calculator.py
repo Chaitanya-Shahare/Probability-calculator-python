9400640dce97f3ea8d94e7d556725e03b1d2932f
CHAITANYA SHAHARE <chaitanyashahare@CHAITANYAs-MacBook-Air.local>
2022-09-03 19:35:18 +0530

    Complete as per the README.md

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,46 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        print(kwargs)
+        self.contents = []
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
+        print(self.contents)
+    
+    def draw(self, number):
+        all_removed = []
+        if number > len(self.contents):
+            return self.contents
+        for i in range(number):
+            removed = self.contents.pop(int(random.random() * len(self.contents)))
+            all_removed.append(removed)
+        return all_removed
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for i in range(num_experiments):
+        expected_copy = copy.deepcopy(expected_balls)
+        hat_copy = copy.deepcopy(hat)
+        colors_gotten = hat_copy.draw(num_balls_drawn)
+
+        for color in colors_gotten:
+            if color in expected_copy:
+                expected_copy[color] -= 1
+
+        if (all( x <= 0 for x in expected_copy.values())):
+            count += 1
+    probability = count / num_experiments
+
+    return probability
+
+
+
+
+
+
+
+
+
