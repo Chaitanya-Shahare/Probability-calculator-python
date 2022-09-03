b8bad4a1fe7a1f063a12677a7d49f2e148419b7c
CHAITANYA SHAHARE <chaitanyashahare@CHAITANYAs-MacBook-Air.local>
2022-09-03 19:36:06 +0530

    .

--- test_module.py
+++ test_module.py
@@ -23,7 +23,7 @@ class UnitTests(unittest.TestCase):
         hat = prob_calculator.Hat(blue=3,red=2,green=6)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
         actual = probability
-        expected = 0.272
+        expected = 0.252
         self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
         hat = prob_calculator.Hat(yellow=5,red=1,green=3,blue=9,test=1)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
